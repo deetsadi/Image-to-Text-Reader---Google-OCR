a4d1a940d97eb63c177975e6acbfee04c85dd74e
Aditya Sridhar <47929718+deetsadi@users.noreply.github.com>
2019-11-24 23:00:45 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from google.cloud import vision
 from google.cloud.vision import types
 
 def detect_text(path):
-    """Detects text in the file."""
+  
     client = vision.ImageAnnotatorClient()
 
     with io.open(path, 'rb') as image_file:
@@ -17,6 +17,7 @@ def detect_text(path):
     response = client.text_detection(image=image)
     texts = [text.description for text in response.text_annotations]
     print('Texts:\n')
+    #more detailed description with dimesions and location for each line of text:
     #for text in texts:
         #print('\n"{}"'.format(text.description))
 
@@ -24,6 +25,4 @@ def detect_text(path):
                     #for vertex in text.bounding_poly.vertices])
 
         #print('bounds: {}'.format(','.join(vertices)))
-#detect_text("/Users/adityasridhar/Downloads/slidenote.jpg")
-detect_text("/Users/adityasridhar/Downloads/letternote.jpg")
-#detect_text("/Users/adityasridhar/Downloads/receipttest.jpg")
+detect_text("receipttest.jpg")
